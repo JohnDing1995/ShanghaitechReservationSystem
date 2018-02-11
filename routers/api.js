9a5768c8b8a9ed58c57e420d7603817dce6f175e
dry950526@gmail.com <dry950526@gmail.com>
2018-02-11 22:17:13 +0800

    api

--- routers/api.js
+++ routers/api.js
@@ -21,15 +21,32 @@ router.get('/seats', function(req, res){
 })
 
 router.post('/reserve', function(req, res){
-    const name = req.body.name;
-    const time = req.body.time;
-    const id = req.body.id;
-    const seat_id = req.body.seat_id
-    const data_crypto = toString(id) + toString(time)
-    const code = crypto.createHash('md5').update(data_crypto).digest("hex");//generate identification code,by student id and booking time
-    ```
-    save above to database(NoSQL is preferred)
-    return outcome through response
-    ```
+    //check seats' number
+    if(req.body.seats.length > 3){
+        responseData.message = 'Maximum 3 seats allowed'
+        responseData.succeed = false
+        res.json(responseData)
+    } else {
+        const name = req.body.name;
+        const time = req.body.time;
+        const id = req.body.id;
+        const seats = req.body.seats
+        const data_crypto = toString(id) + toString(time)
+        const code = crypto.createHash('md5').update(data_crypto).digest("hex");//generate identification code,by student id and booking time
+  
+        //save above to database(NoSQL is preferred)
+        responseData.code = 200
+        responseData.message = "Reservation Succeed!"
+        responseData.data = {
+            name:name,
+            time:time,
+            id:id,
+            seats:seats,
+            code:code
+        }
+        res.json(responseData)
+        //return outcome through response
+    
 
+    }
 })
